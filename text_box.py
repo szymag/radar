7b64cfd62c41f14410b098de771f1a31c77be13c
Szymon Mieszczak <szymon.mieszczak@gmail.com>
2018-04-17 17:35:59 +0200

    Add class which handle text box.

---
+++ text_box.py
@@ -0,0 +1,43 @@
+import pygame
+
+
+pygame.init()
+COLOR_INACTIVE = pygame.Color('lightskyblue3')
+COLOR_ACTIVE = pygame.Color(0, 251, 0, 255)
+FONT = pygame.font.Font(None, 32)
+
+
+class TextBox:
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(*event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN or event.key == pygame.K_TAB:
+                    # TODO: skip to following text box; not sure if it's possible
+                    self.active = not self.active
+                    self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def draw(self, screen):
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+
+
+if __name__ == '__main__':
+    pass
